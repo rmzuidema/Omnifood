ea67dbb4c1deda0b1bafa41fa3de1955995da016
rmzuidema <robert.m.zuidema@gmail.com>
2018-01-06 16:39:53 -0500

    updated javascript

--- resources/js/script.js
+++ resources/js/script.js
@@ -66,5 +66,21 @@ $(document).ready( function(){
         }
       }); 
     
+    $('.js--nav-icon').click(function (){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        if (icon.hasClass('ion-navicon-round')) {
+             icon.removeClass('ion-navicon-round');
+             icon.addClass('ion-close-round');
+        } else {
+             icon.removeClass('ion-close-round');
+             icon.addClass('ion-navicon-round');
+       }
+        nav.slideToggle(200);
+        
+    });
+    
+    
+    
     
 });
